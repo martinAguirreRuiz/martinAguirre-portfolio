7d9c3a280a23528f78320dafcabdf6ba1c8dead1
martinAguirreRuiz <martinfaguirreruiz@gmail.com>
2025-02-26 19:51:05 -0300

    56:40 - Working on the Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./app.scss";
 import Navbar from "./components/navbar/Navbar";
+import Hero from "./components/hero/Hero";
 
 const App = () => {
   return <div>
     <section id="Home">
       <Navbar/>
+      <Hero/>
     </section>
     <section id="Services">Parallax</section>
     <section>Services</section>
