fac7f337b5925fbafa62ad009e81c71b183d2f14
martinAguirreRuiz <martinfaguirreruiz@gmail.com>
2025-02-28 12:19:53 -0300

    1:35:15 Finished with Services, starting Portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./app.scss";
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
+import Services from "./components/services/Services";
 
 const App = () => {
   return <div>
@@ -8,10 +9,10 @@ const App = () => {
       <Navbar/>
       <Hero/>
     </section>
-    <section id="Services">Parallax</section>
-    <section>Services</section>
-    <section id="Portfolio">Parallax</section>
-    <section>Portfolio1</section>
+    {/* <section id="Services">Parallax</section> */}
+    <section id="Services"><Services/></section>
+    {/* <section id="Portfolio">Parallax</section> */}
+    <section id="Portfolio">Portfolio1</section>
     <section>Portfolio2</section>
     <section id="Contact">Contact</section>
   </div>;
