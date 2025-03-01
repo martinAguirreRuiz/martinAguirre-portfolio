4d2a033b6b570fa87598ca1b1f557c83de7aaeb3
martinAguirreRuiz <martinfaguirreruiz@gmail.com>
2025-03-01 13:25:03 -0300

    2:14:44 - Contact finished

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,5 +1,7 @@
 import "./contact.scss";
-import { motion } from "framer-motion";
+import { motion, useInView } from "framer-motion";
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 
 const variants = {
     initial: {
@@ -18,8 +20,28 @@ const variants = {
 
 const Contact = () => {
 
+  const ref = useRef();
+  const formRef = useRef();
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(null);
+
+  const isInView = useInView(ref, {margin: "-100px"});
+
+  const sendEmail = (e) => {
+    
+    e.preventDefault();
+
+    emailjs.sendForm('service_k769hdn', 'template_gw6tzgl', formRef.current, '2nd1kGEoAi5xd_gAp')
+      .then(() => {
+        setSuccess(true);
+      }, () => {
+        setError(true);
+      });
+
+  }
+
   return (
-    <motion.div className="contact" variants={variants} initial="initial" whileInView="animate">
+    <motion.div className="contact" variants={variants} initial="initial" whileInView="animate" ref={ref}>
         <motion.div className="textContainer" variants={variants}>
             <motion.h1 variants={variants}>Let`s work together</motion.h1>
             <motion.div className="item">
@@ -36,14 +58,19 @@ const Contact = () => {
             </motion.div>
         </motion.div>
         <div className="formContainer">
-            <div className="phoneSvg">
+            <motion.div 
+              className="phoneSvg" 
+              initial={{opacity:1}}  
+              whileInView={{opacity:0}}
+              transition={{delay: 3, duration: 1}}
+            >
                 <svg width="450px" height="450px" viewBox="0 0 32.666 32.666">
                     <motion.path
                     strokeWidth={0.2}
                     fill="none"
-                    // initial={{ pathLength: 0 }}
-                    // animate={isInView && { pathLength: 1 }}
-                    // transition={{ duration: 3 }}
+                    initial={{ pathLength: 0 }}
+                    animate={isInView && { pathLength: 1 }}
+                    transition={{ duration: 3 }}
                     d="M28.189,16.504h-1.666c0-5.437-4.422-9.858-9.856-9.858l-0.001-1.664C23.021,4.979,28.189,10.149,28.189,16.504z
                     M16.666,7.856L16.665,9.52c3.853,0,6.983,3.133,6.981,6.983l1.666-0.001C25.312,11.735,21.436,7.856,16.666,7.856z M16.333,0
                     C7.326,0,0,7.326,0,16.334c0,9.006,7.326,16.332,16.333,16.332c0.557,0,1.007-0.45,1.007-1.006c0-0.559-0.45-1.01-1.007-1.01
@@ -59,13 +86,21 @@ const Contact = () => {
                     C32.666,7.326,25.339,0,16.333,0z"
                     />
                 </svg>
-            </div>
-            <form>
-                <input type="text" required placeholder="Name" />
-                <input type="email" required placeholder="Email" />
-                <textarea rows={8} placeholder="Message" />
+            </motion.div>
+            <motion.form
+              ref={formRef}
+              initial={{opacity:0}}  
+              whileInView={{opacity:1}}
+              transition={{delay: 4, duration: 1}}
+              onSubmit={sendEmail}
+            >
+                <input type="text" required placeholder="Name" name="name" />
+                <input type="email" required placeholder="Email" name="email" />
+                <textarea rows={8} placeholder="Message" name="message" />
                 <button>Submit</button>
-            </form>
+                {error && "Error"}
+                {success && "Success"}
+            </motion.form>
         </div>
     </motion.div>
   );
